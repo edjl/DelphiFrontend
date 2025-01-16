dc46d21280eab4954a5b3e47f8d0ac4017a44357
edlee <edlee05072002@gmail.com>
2025-01-16 16:41:04 -0500

    Fix Bet Event Page format

--- delphi_app/lib/bet/bet_event_page.dart
+++ delphi_app/lib/bet/bet_event_page.dart
@@ -4,6 +4,8 @@ import '../model/option.dart';
 import 'get_options_service.dart';
 import '../model/user_profile.dart';
 import '../model/event.dart';
+import '../shared_services/abbreviated_numberstring_format.dart';
+import '../shared_services/date_string_format.dart';
 
 class BetEventPage extends StatefulWidget {
   final Event event; // Add a final field to store the event
@@ -26,11 +28,46 @@ class _BetEventPageState extends State<BetEventPage> {
         widget.event.name);
   }
 
+  // Method to show the pop-up dialog
+  void _showPriceDialog(String priceType, int price) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Selected Price'),
+          content: Text('$priceType: \$${price.toStringAsFixed(2)}'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Close'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.event.name), // Access the event via widget.event
+        title: Flexible(
+          child: Text(
+            "Who will win the 2025 Stanley Cup?", // widget.event.name,
+            style: TextStyle(
+              fontFamily: 'IBM Plex Sans', // Set font to IBM Plex Sans
+              fontSize: 32, // Set font size to 32
+              fontWeight: FontWeight.w600, // Set font weight to semibold
+            ),
+            textAlign: TextAlign.center, // Center align the text
+            maxLines: 3, // Allow up to 2 lines
+            overflow: TextOverflow.ellipsis, // Gracefully handle overflow
+          ),
+        ),
+        centerTitle: true, // Ensures that the title is centered in the AppBar
+        toolbarHeight: 100, // Adjust the height of the AppBar
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -50,89 +87,108 @@ class _BetEventPageState extends State<BetEventPage> {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    // Event image
-                    CachedNetworkImage(
-                      imageUrl: widget.event.topOptionImage ??
-                          "https://i.imgur.com/dRk6nBk.jpeg",
-                      width: double.infinity,
-                      height: 200,
-                      fit: BoxFit.cover,
-                    ),
                     SizedBox(height: 16),
-                    // Event name and details
-                    Text(
-                      widget.event.name,
-                      style:
-                          TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(height: 8),
-                    Text('Shares: ${widget.event.shares}'),
-                    Text('Market Cap: ${widget.event.marketCap}'),
-                    Text('Top Option: ${widget.event.topOptionTitle}'),
-                    Text('Price: \$${widget.event.topOptionPrice}'),
-                    Text('User Bought: ${widget.event.userBought}'),
-                    SizedBox(height: 16),
-                    // Options table
-                    DataTable(
-                      columns: const <DataColumn>[
-                        DataColumn(
-                          label: Text('Option'),
-                        ),
-                        DataColumn(
-                          label: Text('YES'),
-                        ),
-                        DataColumn(
-                          label: Text('NO'),
-                        ),
-                      ],
-                      rows: options.map((option) {
-                        return DataRow(
-                          cells: [
-                            DataCell(Text(option.title)),
-                            DataCell(
-                              ElevatedButton(
-                                onPressed: () {
-                                  // Handle YES button press for option
-                                  print('YES for: ${option.positivePrice}');
-                                },
-                                child: Text('${option.positivePrice}'),
+                    // Event name and details in two columns
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        // Left column with event details
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              '${AbbreviatedNumberstringFormat.formatMarketCap(widget.event.marketCap)} credits',
+                              style: TextStyle(
+                                fontSize: 15, // Set font size to 15
+                                fontFamily:
+                                    'IBM Plex Sans', // Set font to IBM Plex Sans
+                                fontWeight:
+                                    FontWeight.w500, // Set medium boldness
                               ),
                             ),
-                            DataCell(
-                              ElevatedButton(
-                                onPressed: () {
-                                  // Handle NO button press for option
-                                  print('NO for: ${option.negativePrice}');
-                                },
-                                child: Text('${option.negativePrice}'),
+                            Text(
+                              '${AbbreviatedNumberstringFormat.formatShares(widget.event.shares)} shares',
+                              style: TextStyle(
+                                fontSize: 15, // Set font size to 15
+                                fontFamily:
+                                    'IBM Plex Sans', // Set font to IBM Plex Sans
+                                fontWeight:
+                                    FontWeight.w500, // Set medium boldness
+                              ),
+                            ),
+                            Text(
+                              'Ends ${DateStringFormat.formatEndDate(widget.event.endDate)}',
+                              style: TextStyle(
+                                fontSize: 15, // Set font size to 15
+                                fontFamily:
+                                    'IBM Plex Sans', // Set font to IBM Plex Sans
+                                fontWeight:
+                                    FontWeight.w500, // Set medium boldness
                               ),
                             ),
                           ],
+                        ),
+                        // Right column with My Shares button
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            ElevatedButton(
+                              onPressed: () {
+                                // Handle "My Shares" button press
+                                print('My Shares pressed');
+                              },
+                              child: Text('My Shares'),
+                            ),
+                            SizedBox(height: 8),
+                            Text('YES', style: TextStyle(fontSize: 16)),
+                            Text('NO', style: TextStyle(fontSize: 16)),
+                          ],
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 16),
+                    // Options list
+                    Column(
+                      children: options.map((option) {
+                        return Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 8.0),
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              // Option image
+                              CachedNetworkImage(
+                                imageUrl: option.imageLink ??
+                                    "https://i.imgur.com/dRk6nBk.jpeg",
+                                width: 29,
+                                height: 29,
+                                fit: BoxFit.cover,
+                              ),
+                              SizedBox(width: 16),
+                              // Option title
+                              Text(
+                                option.title,
+                                style: TextStyle(fontSize: 16),
+                              ),
+                              SizedBox(width: 16),
+                              // Positive price button
+                              ElevatedButton(
+                                onPressed: () {
+                                  _showPriceDialog('YES', option.positivePrice);
+                                },
+                                child: Text('\$${option.positivePrice}'),
+                              ),
+                              SizedBox(width: 8),
+                              // Negative price button
+                              ElevatedButton(
+                                onPressed: () {
+                                  _showPriceDialog('NO', option.negativePrice);
+                                },
+                                child: Text('\$${option.negativePrice}'),
+                              ),
+                            ],
+                          ),
                         );
                       }).toList(),
-                      // rows: snapshot.data!.map((option) {
-                      //   return DataRow(children: [
-                      //     DataCell([Text(option.title)]),
-                      //     DataCell(
-                      //       Row (
-                      //         mainAxisAlignment: MainAxisAlignment.center,
-                      //         children: [
-                      //         ElevatedButton (
-                      //             onPressed: () {},
-                      //             child: const Text(options[index].negativePrice),
-                      //         )],
-                      //     )),
-                      //     DataCell(
-                      //       Row(
-                      //         mainAxisAlignment: MainAxisAlignment.center,
-                      //         children: [
-                      //         ElevatedButton (
-                      //             onPressed: () {},
-                      //             child: const Text(options[index].negativePrice),
-                      //         )],
-                      //     )),
-                      //   ]).toList();
-                      // })
                     ),
                   ],
                 ),
