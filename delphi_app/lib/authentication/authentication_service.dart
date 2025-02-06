682dd7d7321d94df8b6f910649f4680cd26e1023
edlee <edlee05072002@gmail.com>
2025-02-06 16:39:39 -0500

    Signup hooked up

--- delphi_app/lib/authentication/authentication_service.dart
+++ delphi_app/lib/authentication/authentication_service.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import '../model/account_creation_response.dart';
 import '../model/login_response.dart';
 
 class AuthenticationService {
@@ -20,6 +21,32 @@ class AuthenticationService {
     if (response.statusCode == 200) {
       final Map<String, dynamic> responseData = json.decode(response.body);
       return LoginResponse.fromJson(responseData);
+    } else {
+      return null;
+    }
+  }
+
+  static Future<AccountCreationResponse?> signup(
+      String username, String email, String password) async {
+    final url = Uri.parse('https://user.fleure.workers.dev/api/CreateAccount');
+    final response = await http.post(
+      url,
+      headers: {
+        'accept': '*/*',
+        'Content-Type': 'application/json',
+      },
+      body: json.encode({
+        'username': username,
+        'email': email,
+        'password': password,
+      }),
+    );
+
+    if (response.statusCode == 200 ||
+        response.statusCode == 400 ||
+        response.statusCode == 500) {
+      final Map<String, dynamic> responseData = json.decode(response.body);
+      return AccountCreationResponse.fromJson(responseData);
     } else {
       return null;
     }
