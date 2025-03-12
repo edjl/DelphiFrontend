62863326205bed01c19d30a35268541888ca6df7
edlee <edlee05072002@gmail.com>
2025-03-12 12:17:14 -0400

    chrome

--- delphi_app/lib/profile/user_profile_service.dart
+++ delphi_app/lib/profile/user_profile_service.dart
@@ -1,49 +1,13 @@
-import 'package:shared_preferences/shared_preferences.dart';
 import '../model/user_profile.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert'; // For JSON decoding
 
 class UserProfileService {
   Future<void> loadUserProfile() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-
-    int userId = prefs.getInt('userId') ?? -1;
-    if (userId != -1) {
-      UserProfile().userId.value = userId;
-      UserProfile().username = prefs.getString('username') ?? "DefaultUser";
-      UserProfile().balance.value = prefs.getInt('balance') ?? 0;
-      UserProfile().isAdmin = prefs.getBool('isAdmin') ?? false;
-      UserProfile().bankruptcyCount = prefs.getInt('bankruptcyCount') ?? 0;
-      UserProfile().totalBets = prefs.getInt('totalBets') ?? 0;
-      UserProfile().currentBets = prefs.getInt('currentBets') ?? 0;
-      UserProfile().totalCreditsPlaying =
-          prefs.getInt('totalCreditsPlaying') ?? 0;
-      UserProfile().totalCreditsBet = prefs.getInt('totalCreditsBet') ?? 0;
-      UserProfile().totalCreditsWon = prefs.getInt('totalCreditsWon') ?? 0;
-      UserProfile().premiumAccount = prefs.getBool('premiumAccount') ?? false;
-      UserProfile().profitMultiplier = prefs.getInt('profitMultiplier') ?? 100;
-    }
-
     refresh();
-    saveUserProfile();
   }
 
-  Future<void> saveUserProfile() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-
-    prefs.setInt('userId', UserProfile().userId.value);
-    prefs.setString('username', UserProfile().username);
-    prefs.setBool('isAdmin', UserProfile().isAdmin);
-    prefs.setInt('balance', UserProfile().balance.value);
-    prefs.setInt('bankruptcyCount', UserProfile().bankruptcyCount);
-    prefs.setInt('totalBets', UserProfile().totalBets);
-    prefs.setInt('currentBets', UserProfile().currentBets);
-    prefs.setInt('totalCreditsPlaying', UserProfile().totalCreditsPlaying);
-    prefs.setInt('totalCreditsBet', UserProfile().totalCreditsBet);
-    prefs.setInt('totalCreditsWon', UserProfile().totalCreditsWon);
-    prefs.setBool('premiumAccount', UserProfile().premiumAccount);
-    prefs.setInt('profitMultiplier', UserProfile().profitMultiplier);
-  }
+  Future<void> saveUserProfile() async {}
 
   void refresh() async {
     try {
